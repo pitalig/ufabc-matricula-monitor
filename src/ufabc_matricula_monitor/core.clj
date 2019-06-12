09bc67faacafba47e8a0aeb59373d2ed1a8dbb83
Gabriel Pitali <gabriel.pitali@nubank.com.br>
2019-06-12 11:34:40 -0300

    Update important ids

--- src/ufabc_matricula_monitor/core.clj
+++ src/ufabc_matricula_monitor/core.clj
@@ -102,6 +102,11 @@
 
 (def important-ids
   #{648 ; Laboratório de Física I A-Noturno (Santo André)
+    20 ; Sistemas CAD/CAE A1 noturno
+    402 ; Sistemas de Informação Corporativos A noturno
+    301 ; Pesquisa Operacional A noturno
+    370 ; Modelos e Ferramentas de Gestão Ambiental A noturno
+    417 ; Inovação Estratégica A noturno
     })
 
 (defn alert! [disciplina open-slots]
